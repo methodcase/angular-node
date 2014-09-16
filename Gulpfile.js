911f9b87d1543338ef5e2b962c6fcceffce139f6
Your Name <you@example.com>
2014-09-16 16:38:52 +0800

    ''

--- Gulpfile.js
+++ Gulpfile.js
@@ -11,8 +11,10 @@ gulp.task('default',function () {
 	app.use(express.static(__dirname));
 
 	app.listen(3100); // express server
+	console.log("Server running at http://localhost:3100/");
 
 	lr.listen(35729); // tiny-lr server	
+	console.log("tiny-lr server running at port 35729");
 
 	gulp.watch('*',function(event){		
 		var fileName = require('path').relative(__dirname, event.path);
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This is a starter template to get JavaScript up and running.
+	npm install
+	gulp
+
+Browse in http://localhost:3100/
