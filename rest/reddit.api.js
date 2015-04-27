a3cc9d903a927986e2d7adf3c9b74b45b57cc253
methodcase <methodcase@gmail.com>
2015-04-27 11:22:28 +0800

    all

--- rest/reddit.api.js
+++ rest/reddit.api.js
@@ -1,37 +1,36 @@
 module.exports = function(ress){
-
 	var http = require('http');
 	var options = {
-		'hostname': 'api.reddit.com',
-		'port': 80,
-		'path': '/hot',
-		'method': 'GET',
-		'headers': {'user-agent': 'Mozilla/5.0'}
-	}	
-
+	'hostname': 'api.reddit.com',
+	'port': 80,
+	'path': '/hot',
+	'method': 'GET',
+	'headers': {'user-agent': 'Mozilla/5.0'}
+	}
 	var obj = {};
-	
+
+
 	var callBack = function(d){
 		processed_data = [];
-		d.data.children.map(function(a){		
+		d.data.children.map(function(a){
 			processed_data.push(a.data.title)
 		});
-
+		obj = processed_data;
 		return processed_data;
 	}
 
-	http.get(options, function(res){	
-		console.log(res.statusCode)	
-		var body  = '';		
+	http.get(options, function(res){
+		console.log(res.statusCode)
+		var body = '';
 		res.on('data', function(chunk){
 			body += chunk
 		});
 		res.on('end', function(){
-			var info  = JSON.parse(body);
+			var info = JSON.parse(body);
 			callBack(info);
 		});
 	}, callBack);
 
-	console.log(obj)	 //no data returned
+	console.log(obj) //no data returned
 	ress.send(obj)
 }
