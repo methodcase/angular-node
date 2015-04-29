1f86033862957b70338f541d4496e9f304783776
methodcase <methodcase@gmail.com>
2015-04-29 11:45:39 +0800

    angular-node

--- rest/router.js
+++ rest/router.js
@@ -4,23 +4,9 @@ module.exports = function(app){
 		res.render('index.html')
 	});
 
-	app.get('/someapi', function(req, res){
-		
-		res.send("{name:'Funny'}");
+	app.get("/rest/article", function(req, res){
+		var article = require('./article.api.js')
+		console.log(article.show(res))
 	});
-
-	app.post("/upload", function(){
-		console.log('uploading..');
-	});
-
-	app.get("/rest/show", function(req, res){
-		var reddit = require('./reddit.api.js');
-		reddit(res);
-	});
-
-	app.get("/rest/show2", function(){
-		
-	});
-
-}
 	
+}
--- www/js/app.controllers.js
+++ www/js/app.controllers.js
@@ -1,7 +1,13 @@
 angular.module('app.controllers',[])
 
-.controller('HomeCtrl', function($scope){
+.controller('HomeCtrl', function($scope, $http){
 
-	
+	$http.get('/rest/article').
+	  success(function(data, status, headers, config) {     
+	  	$scope.articles = data;
+	  }).
+	  error(function(data, status, headers, config) {
+	  });
+
 })
 
